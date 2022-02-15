f5245697ace153d8104c3569ceddf6757af94651
Gavan Lamb <gavan.jon.lamb@gmail.com>
2022-02-15 22:54:14 +1100

    init

---
+++ MultiCarrier.Api/WeatherForecast.cs
@@ -0,0 +1,12 @@
+namespace MultiCarrier.Api;
+
+public class WeatherForecast
+{
+    public DateTime Date { get; set; }
+
+    public int TemperatureC { get; set; }
+
+    public int TemperatureF => 32 + (int) (TemperatureC / 0.5556);
+
+    public string? Summary { get; set; }
+}
